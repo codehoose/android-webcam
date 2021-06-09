8de47a511c70ce95ea2fe81c564e3a406561b730
Sloan Kelly <sloan@codehoose.com>
2021-06-08 21:53:24 -0400

    An old web cam project from 2017!

---
+++ Assets/Scripts/WebCam.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WebCam : MonoBehaviour
+{
+    int currentCamIndex = 0;
+
+    WebCamTexture tex;
+
+    public RawImage display;
+
+    public Text startStopText;
+
+    public Text debug;
+
+    public void SwapCam_Clicked()
+    {
+        if (WebCamTexture.devices.Length > 0)
+        {
+            currentCamIndex += 1;
+            currentCamIndex %= WebCamTexture.devices.Length;
+
+            // if tex is not null:
+            // stop the web cam
+            // start the web cam
+
+            if (tex != null)
+            {
+                StopWebCam();
+                StartStopCam_Clicked();
+            }
+        }
+    }
+
+    public void StartStopCam_Clicked()
+    {
+        if (tex != null) // Stop the camera
+        {
+            StopWebCam();
+            startStopText.text = "Start Camera";
+        }
+        else // Start the camera
+        {
+            WebCamDevice device = WebCamTexture.devices[currentCamIndex];
+            tex = new WebCamTexture(device.name);
+            display.texture = tex;
+
+            tex.Play();
+            startStopText.text = "Stop Camera";
+        }
+    }
+
+    private void StopWebCam()
+    {
+        display.texture = null;
+        tex.Stop();
+        tex = null;
+    }
+}
